e6b7febd99c4d13fa57579b10eb9d0fe80b87cdb
Ooi Qi Bin <p22014403@student.newinti.edu.my>
2026-06-06 18:32:23 +0800

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,38 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+
+import Layout from "./components/Layout";
 import DashboardPage from "./pages/DashboardPage";
+import TripsPage from "./pages/TripsPage";
+import CreateTripPage from "./pages/CreateTripPage";
+import ItineraryPage from "./pages/ItineraryPage";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* AUTH */}
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
+
+        {/* APP */}
+        <Route element={<Layout />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/trips" element={<TripsPage />} />
+
+          {/* CREATE */}
+          <Route path="/create-trip" element={<CreateTripPage />} />
+
+          {/* EDIT (reuse CreateTripPage) */}
+          <Route path="/edit-trip/:id" element={<CreateTripPage />} />
+          <Route path="/view-trip/:id" element={<CreateTripPage />} />
+
+          <Route path="/Itinerary" element={<ItineraryPage />} />
+        </Route>
+
       </Routes>
     </BrowserRouter>
   );
